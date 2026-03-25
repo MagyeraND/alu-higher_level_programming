5add28fa461b4478edf20af598b172ae279ebf71
MagyeraND <d.nkubito1@alustudent.com>
2026-03-25 04:56:34 +0200

    Update 6-rectangle.py

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -1,11 +1,68 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class with instance counter."""
+
+
 class Rectangle:
-    number_of_instances = 0  # Class attribute
+    """Represents a rectangle."""
+
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        Rectangle.number_of_instances += 1
+        """Increment count on init."""
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
+    @property
+    def width(self):
+        """Get width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        """String."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rect = [("#" * self.__width) for _ in range(self.__height)]
+        return "\n".join(rect)
+
+    def __repr__(self):
+        """Repr."""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
     def __del__(self):
-        Rectangle.number_of_instances -= 1
+        """Decrement count and print message."""
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
