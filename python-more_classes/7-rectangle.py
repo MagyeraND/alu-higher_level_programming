d1eaab981adf59443e3d34b6b76a59244747378c
MagyeraND <d.nkubito1@alustudent.com>
2026-03-25 04:57:02 +0200

    Update 7-rectangle.py

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -1,10 +1,70 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class with customizable symbol."""
+
+
 class Rectangle:
+    """Represents a rectangle."""
+
     number_of_instances = 0
     print_symbol = "#"
 
+    def __init__(self, width=0, height=0):
+        """Init."""
+        type(self).number_of_instances += 1
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
     def __str__(self):
+        """String with symbol."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        # Use str(self.print_symbol) to handle lists/tuples passed as symbols
-        line = str(self.print_symbol) * self.__width
-        return ((line + "\n") * self.__height)[:-1]
+        symbol = str(self.print_symbol)
+        rect = [(symbol * self.__width) for _ in range(self.__height)]
+        return "\n".join(rect)
+
+    def __repr__(self):
+        """Repr."""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Del."""
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
