abba1c079f73048bfcc06f259ed6b8a26950224c
MagyeraND <d.nkubito1@alustudent.com>
2026-03-25 04:03:46 +0200

    w

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+    def __init__(self, size):
+        """Initializes the square.
+        Args:
+            size: The length of a side of the square.
+        """
+        self.__size = size
